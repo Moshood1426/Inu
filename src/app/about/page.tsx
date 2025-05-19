ef06b9bafc3fd4d5aaecf062ddb59f7f3155cee0
Moshood1426 <moshood.yemi10@gmail.com>
2025-05-19 06:41:33 -0600

    canges

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,15 @@
+import About from "@/components/about/about";
+import Nav from "@/components/general/nav/Nav";
 import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
-    <div
-      style={{
-        width: "90%",
-        margin: "0 auto",
-        textAlign: "center",
-        padding: "10vh 0",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <h3>About Us Page</h3>
-      <Link href={"/"}>
-        <button className="btn btn_light">Go Home</button>
-      </Link>
+    <div>
+      <div className="nav small-landing">
+        <Nav />
+      </div>
+      <About />
     </div>
   );
 };
---
+++ src/components/about/about.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import classes from "./about.module.scss";
+
+const About = () => {
+  return (
+    <div className={classes.about}>
+      <div className={classes.about_container}>
+        <div className="big-landing">
+          <span>Back</span>
+          
+        </div>
+        <div>
+          <h3>Learn More About Us</h3>
+          <p>Our Inspiration, community and mission</p>
+        </div>
+
+        <section>
+          <h5>Welcome to Archiversehub</h5>
+          <p>
+            Archiversehub is a platform for exploring the ever-evolving world of
+            architecture and design. We&apos;re passionate about uncovering the
+            stories behind the built environment, from iconic landmarks to
+            hidden gems, and sharing them with a community of like-minded
+            individuals.{" "}
+          </p>
+        </section>
+
+        <section>
+          <h5>Our Mission</h5>
+          <p>
+            At Archiversehub, we believe that architecture has the power to
+            shape our experiences, influence our cultures, and impact our
+            planet. Our mission is to inspire, educate, and spark meaningful
+            conversations about the role of architecture in our lives
+          </p>
+        </section>
+
+        <section>
+          <h5>What You&apos;ll Find Here</h5>
+          <ul>
+            <li>
+              In-depth articles and analysis on architectural trends,
+              technologies, and innovations
+            </li>
+            <li>Stories of remarkable buildings, architects, and designers </li>
+            <li>
+              Insights into sustainable design, urban planning, and social
+              impact{" "}
+            </li>
+            <li>
+              A curated selection of inspiring projects, images, and resource
+            </li>
+          </ul>
+        </section>
+
+        <section>
+          <h5>Our Approach</h5>
+          <p>
+            We&apos;re committed to fostering a community that values
+            creativity, critical thinking, and curiosity. Our content is crafted
+            to be informative, engaging, and thought-provoking, with a focus on
+            showcasing diverse perspectives and experiences.{" "}
+          </p>
+        </section>
+
+        <section>
+          <h5>Join The Conversation</h5>
+          <p>
+            Whether you&apos;re an architect, designer, student, or simply
+            someone who appreciates the built environment, we invite you to join
+            our community. Share your thoughts, ask questions, and let&apos;s
+            explore the world of architecture together.
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default About;
